3dae8db174d93f3890e927edf22470919174cda4
Rodrigo <rodrigorael53@gmail.com>
2022-07-01 17:18:54 -0300

    creating a button using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,31 @@ const Button = styled('button', {
 
 function App() {
   return (
-    <div>
-      <Button size="big">Hello World</Button>
-      <Button>Hello World</Button>
-      <Button size="small">Hello World</Button>
+    // <div>
+    //   <Button size="big">Hello World</Button>
+    //   <Button>Hello World</Button>
+    //   <Button size="small">Hello World</Button>
+    // </div>
+
+    <div className="h-screen flex justify-center items-center dark:bg-zinc-900">
+      {
+      /*
+        no caso do tailwind a estilização vai ser feita utilizando classes
+        asim com é feito no CSS tradicional
+        mas as classes são classes utilitárias, ou seja, cada classe vai aplicar
+        um estilo diferente
+      */
+      }
+
+      {
+      /* 
+        os tamanhos no tailwind já são em rem, ou seja, isso já ajuda a trabalhar
+        com acessibilidade
+      */
+      }
+      <button className="w-full mx-4 sm:w-auto text-4xl bg-violet-500 py-4 px-8 rounded-lg text-white shadow-lg shadow-violet-400 hover:bg-violet-700 transition-colors animate-bounce">
+        Hello World
+      </button>
     </div>
   );
 }
