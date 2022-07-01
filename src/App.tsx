c412fbbbccfaf2cc78fddcac36f2db05564f44fb
Rodrigo <rodrigorael53@gmail.com>
2022-07-01 17:33:29 -0300

    using RadixUI to accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import styled, { css } from 'styled-components';
 import { styled } from '@stitches/react';
+import * as Popover from '@radix-ui/react-popover';
 
 // interface ButtonProps {
 //   size: 'big' | 'small';
@@ -47,26 +48,31 @@ function App() {
     //   <Button size="small">Hello World</Button>
     // </div>
 
-    <div className="h-screen flex justify-center items-center dark:bg-zinc-900">
-      {
-      /*
-        no caso do tailwind a estilização vai ser feita utilizando classes
-        asim com é feito no CSS tradicional
-        mas as classes são classes utilitárias, ou seja, cada classe vai aplicar
-        um estilo diferente
-      */
-      }
+    <Popover.Root>
+      <div className="h-screen flex flex-col gap-8 justify-center items-center dark:bg-zinc-900">
+        <Popover.Content>
+          <p className="text-white">Olá mundo</p>
+        </Popover.Content>
+        {
+        /*
+          no caso do tailwind a estilização vai ser feita utilizando classes
+          asim com é feito no CSS tradicional
+          mas as classes são classes utilitárias, ou seja, cada classe vai aplicar
+          um estilo diferente
+        */
+        }
 
-      {
-      /* 
-        os tamanhos no tailwind já são em rem, ou seja, isso já ajuda a trabalhar
-        com acessibilidade
-      */
-      }
-      <button className="w-full mx-4 sm:w-auto text-4xl bg-violet-500 py-4 px-8 rounded-lg text-white shadow-lg shadow-violet-400 hover:bg-violet-700 transition-colors animate-bounce">
-        Hello World
-      </button>
-    </div>
+        {
+        /* 
+          os tamanhos no tailwind já são em rem, ou seja, isso já ajuda a trabalhar
+          com acessibilidade
+        */
+        }
+        <Popover.Trigger className="w-full mx-4 sm:w-auto text-4xl bg-violet-500 py-4 px-8 rounded-lg text-white shadow-lg shadow-violet-400 hover:bg-violet-700 transition-colors animate-bounce">
+          Hello World
+        </Popover.Trigger>
+      </div>
+    </Popover.Root>
   );
 }
 
