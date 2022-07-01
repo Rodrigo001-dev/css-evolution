b9c930cf39956b3c50d3a4170a9b316e04f0d0a8
Rodrigo <rodrigorael53@gmail.com>
2022-07-01 01:22:21 -0300

    create a Button component with Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import styled, { css } from 'styled-components';
+
+interface ButtonProps {
+  size: 'big' | 'small';
+};
+
+const Button = styled.button<ButtonProps>`
+  background-color: pink;
+  padding: 12px;
+  border-radius: 8px;
+  border: 1px solid purple;
+
+  ${props => props.size === 'big' && css`
+    font-size: 64px;
+    padding: 40px;
+  `}
+`;
 
 function App() {
- return (
-  <h1>Hello World</h1>
- );
+  return (
+    <div>
+      <Button size="big">Hello World</Button>
+      <Button size="small">Hello World</Button>
+    </div>
+  );
 }
 
 export default App
